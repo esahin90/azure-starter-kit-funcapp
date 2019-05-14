cb920d62807c3a2c76b75f7c31ce4097d69e9ecd
Emre Sahin <emre.tr@outlook.com>
2019-05-14 15:47:48 +0200

    added trigger to light api

--- AdtEventHubTrigger/index.js
+++ AdtEventHubTrigger/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, eventHubMessages) {
     context.log(`JavaScript eventhub trigger function called for message array ${eventHubMessages}`);
-    
+
     eventHubMessages.forEach((message, index) => {
         // https://docs.microsoft.com/en-us/azure/digital-twins/how-to-egress-endpoints#event-types
         context.log(`Process message`);
@@ -10,7 +10,23 @@ module.exports = async function (context, eventHubMessages) {
         if (context.bindingData.propertiesArray[index].Type == `SpaceChange` &&
             message.DataType == `Humidity` &&
             message.Value > 60 ) {
-            // TODO: call Zumtobel API
+            var ip_to_devices = [['100.100.200.54', '0c418f7a-03eb-4876-9dee-b14da656a162'],
+                                 ['100.100.200.52', '95a8f243-21ba-4a49-b430-5949e74daf5d'],
+                                 ['100.100.200.51', '5acc063b-e3df-43f0-903b-7b9450e9c4c2'],
+                                 ['100.100.200.53', '278a7a11-02ae-47c9-98f0-b36dd62cb3e4']];
+            const url = 'https://cb-zumtobel-lms-websocket.azurewebsites.net/api/v1/controllers/zumtobel-lms/devices/';
+            var header = {token: "1"};
+            const cmd = "/lightingcapability/setintensity/";
+
+            for (var i=0; i < ip_to_devices.length; i++){
+              header['lightNetworkAddress'] = ip_to_devices[i][0];
+              fetch(url + ip_to_devices[i][0] + cmd + '100/3', {headers: header, method:"POST"})
+            }
+
+            for (var i=0; i < ip_to_devices.length; i++){
+              header['lightNetworkAddress'] = ip_to_devices[i][0];
+              fetch(url + ip_to_devices[i][0] + cmd + '0/3', {headers: header, method:"POST"})
+            }
         }
     });
 };
